294403fc3dff04bba519d51fce1777a368340321
mmi366127 <mmi366127@gmail.com>
2023-06-08 05:28:03 +0800

    still fixing trpo...

--- algorithm/trpo.py
+++ algorithm/trpo.py
@@ -1,7 +1,8 @@
 # modified from https://github.com/ikostrikov/pytorch-trpo/blob/master/main.py 
-# and  https://github.com/ikostrikov/pytorch-trpo/blob/master/conjugate_gradients.py
+# and           https://github.com/ikostrikov/pytorch-trpo/blob/master/conjugate_gradients.py
+
 from utils import set_flat_params_to, get_flat_params_from, normal_log_density, get_flat_grad_from
-from torch.autograd import Variable
+# from torch.autograd
 import numpy as np
 import scipy
 import torch
@@ -15,51 +16,24 @@ def flat_grad(y, x, retain_graph=False, create_graph=False):
     g = torch.cat([t.contiguous().view(-1) for t in g])
     return g
 
-def conjugate_gradients(A, b, delta=0., max_iterations=10):
-    x = torch.zeros_like(b)
+
+def conjugate_gradients(Avp, b, nsteps, residual_tol=1e-10):
+    x = torch.zeros(b.size())
     r = b.clone()
     p = b.clone()
-
-    i = 0
-    while i < max_iterations:
-        AVP = A(p)
-
-        dot_old = r @ r
-        alpha = dot_old / (p @ AVP)
-
-        x_new = x + alpha * p
-
-        if (x - x_new).norm() <= delta:
-            return x_new
-
-        i += 1
-        r = r - alpha * AVP
-
-        beta = (r @ r) / dot_old
-        p = r + beta * p
-
-        x = x_new
+    rdotr = torch.dot(r, r)
+    for i in range(nsteps):
+        _Avp = Avp(p)
+        alpha = rdotr / torch.dot(p, _Avp)
+        x += alpha * p
+        r -= alpha * _Avp
+        new_rdotr = torch.dot(r, r)
+        betta = new_rdotr / rdotr
+        p = r + betta * p
+        rdotr = new_rdotr
+        if rdotr < residual_tol:
+            break
     return x
-
-
-# def conjugate_gradients(Avp, b, nsteps, residual_tol=1e-10):
-#     x = torch.zeros_like(b)
-#     r = b.clone()
-#     p = b.clone()
-#     rdotr = torch.dot(r, r)
-#     for i in range(nsteps):
-#         _Avp = Avp(p)
-#         alpha = rdotr / torch.dot(p, _Avp)
-#         x += alpha * p
-#         r -= alpha * _Avp
-#         new_rdotr = torch.dot(r, r)
-#         betta = new_rdotr / rdotr
-#         p = r + betta * p
-#         rdotr = new_rdotr
-#         if rdotr < residual_tol:
-#             break
-#     return x
-
 
 def linesearch(model, f, x, fullstep,
                expected_improve_rate,
@@ -101,58 +75,17 @@ def compute_TRPO_loss_pi(self, data, inter_nu, use_cv, plot=True):
 
 # The KL-divergence between two gaussian distribution
 def kl_div_gaussian(pi_0, std_0, log_std_0, pi_1, std_1, log_std_1):
-    # treat as constant 
-    pi_0 = pi_0.detach()
-    std_0 = std_0.detach()
-    log_std_0 = log_std_0.detach()
-
-    kl = log_std_1 - log_std_0 + (std_0.pow(2) + (pi_0 - pi_1).pow(2)) / (2.0 / std_1.pow(2)) - 0.5    
-    return kl.mean()
+    return log_std_1 - log_std_0 + (std_0.pow(2) + (pi_0 - pi_1).pow(2)) / (2.0 * std_1.pow(2)) - 0.5
 
 
 # The KL-divergence between two categorical distribution
 def kl_div_categorical(dist_0, dist_1):
-    dist_0 = dist_0.detach()
-
-    kl = (dist_0 * (dist_0.log() - dist_1.log()))
-    return kl.mean()
-
-# def get_kl():
-#     # calculate KL divergence between pi_old and pi_new with grad
-#     pi_1 = ac.pi.mu_net(Variable(obs))
-#     log_std_1 = ac.pi.log_std.expand_as(pi_1)
-#     std_1 = torch.exp(log_std_1)
-
-#     pi_0 = Variable(pi_1.data)
-#     log_std_0 = Variable(log_std_1.data)
-#     std_0 = Variable(std_1.data)
-
-#     kl = log_std_1 - log_std_0 + (std_0.pow(2) + (pi_0 - pi_1).pow(2)) / (2.0 / std_1.pow(2)) - 0.5
-
-#     return kl.sum(1, keepdim=True)
-
-
+    return (dist_0 * (dist_0.log() - dist_1.log()))
 
 
 def surrogate_loss(new_log_prob, old_log_prob, adv, learning_signal=1.0):
     # the surrogate function L_{\pi_{\theta_k}} 
-    return (torch.exp(new_log_prob - old_log_prob) * adv.detach() * learning_signal).mean()
-
-# def get_loss(volatile=False):
-#     # the surrogate function L_{\pi_{\theta_k}} 
-#     if volatile:
-#         with torch.no_grad():   
-#             pi = ac.pi.mu_net(Variable(obs))
-#             log_stds = ac.pi.log_std.expand_as(pi)
-#             stds = torch.exp(log_stds)
-#     else:
-#         pi = ac.pi.mu_net(Variable(obs))
-#         log_stds = ac.pi.log_std.expand_as(pi)
-#         stds = torch.exp(log_stds)
-
-#     log_prob = normal_log_density(Variable(act), pi, log_stds, stds)
-#     act_loss = -Variable(adv) * torch.exp(log_prob - Variable(fixed_log_prob))
-#     return act_loss.mean()
+    return -(torch.exp(new_log_prob - old_log_prob) * adv * learning_signal).mean()
 
 
 # Set up function for TRPO update
@@ -201,90 +134,63 @@ def trpo_update(ac, data_on, data_off, inter_nu, use_cv, beta, max_kl, damping):
     # Although original code uses the LBFGS to optimize the value loss,
     # we do it in core.py through gradient descent 
     if ac.discrete:
-        raise NotImplementedError("no categorical implementation for trpo")
+        # raise NotImplementedError("no categorical implementation for trpo")
+        dist = ac.pi(obs)
+        dist = torch.distributions.utils.clamp_probs(dist)
+
+        prob = dist[range(dist.shape[0   ]), act]
+
+
     else:
-        pi = ac.pi.mu_net(obs.detach())
+        pi = ac.pi.mu_net(obs)
         log_stds = ac.pi.log_std.expand_as(pi)
         stds = torch.exp(log_stds)
         
         # The log probability of the actions
-        log_prob = normal_log_density(act.detach(), pi, log_stds, stds)
+        log_prob = normal_log_density(act, pi, log_stds, stds)
         fixed_log_prob = log_prob.detach()
 
-        # loss and KL-divergence variable for gradient calculation
-        L = surrogate_loss(log_prob, fixed_log_prob, adv)
-        KL = kl_div_gaussian(pi, stds, log_stds, pi, stds, log_stds)
+        # create calculation graph for grad to cal grad_grad
+        KL = kl_div_gaussian(pi.detach(), stds.detach(), log_stds.detach(), pi, stds, log_stds)
+        grad_KL = torch.autograd.grad(KL.mean(), ac.pi.parameters(), retain_graph=True, create_graph=True)
+        flat_grad_KL = torch.cat([grad.view(-1) for grad in grad_KL])
+
+
+        def Fvp(v):
+            KL_v = (flat_grad_KL * v).sum()
+            grad_KL_v = torch.autograd.grad(KL_v, ac.pi.parameters(), retain_graph=True)
+            flag_grad_KL_v = torch.cat([grad.contiguous().view(-1) for grad in grad_KL_v]).detach()
+
+            return flag_grad_KL_v + v * damping
+
+
+        def get_loss():
+            with torch.no_grad():
+                pi = ac.pi.mu_net(obs)
+                log_stds = ac.pi.log_std.expand_as(pi)
+                stds = torch.exp(log_stds)
+            
+            log_prob = normal_log_density(act, pi, log_stds, stds)
+            return surrogate_loss(log_prob, fixed_log_prob, adv).detach()
         
-        # policy parameters
-        parameters = list(ac.pi.parameters())
 
-        # Retain, because we will use the graph several times
-        g = flat_grad(L, parameters, retain_graph=True)  
+        loss = surrogate_loss(log_prob, fixed_log_prob, adv)
+        grad_loss = torch.autograd.grad(loss, ac.pi.parameters(), retain_graph=True)
+        flat_grad_loss = torch.cat([grad.view(-1) for grad in grad_loss]).detach()
+        print(flat_grad_loss.shape)
 
-        # Create graph, because we will call backward() on the graph itself (for hessian-vector product)
-        d_kl = flat_grad(KL, parameters, create_graph=True)
-        
-        print(f'g: {g.shape}, d_kl{d_kl.shape}')
+        stepdir = conjugate_gradients(Fvp, -flat_grad_loss, 10)
 
-        def fvp(v):
-            return flat_grad(d_kl @ v, parameters, retain_graph=True) #+ v * damping
+        shs = 0.5 * (stepdir * Fvp(stepdir)).sum(0, keepdim=True)
 
-        stepdir = conjugate_gradients(fvp, g)
-        max_kl = 0.05
-        lm = torch.sqrt(2.0 * max_kl / (stepdir @ fvp(stepdir)))
-        neggdotstepdir = (g * stepdir).sum(0, keepdim=True)
-        fullstep = lm * stepdir
+        lm = torch.sqrt(shs / max_kl)
+        fullstep = stepdir / lm[0]
 
-        print('here-->', lm, fullstep, neggdotstepdir)
-        
-        print(("lagrange multiplier:", lm.item(), "grad_norm:", g.norm()))
+        neggdotstepdir = (-flat_grad_loss * stepdir).sum(0, keepdim=True)
+        print(("lagrange multiplier:", lm[0], "grad_norm:", flat_grad_loss.norm()))
 
         prev_params = get_flat_params_from(ac.pi)
 
-        def f():
-            with torch.no_grad():
-                log_prob = normal_log_density(act, pi, log_stds, stds)
-                act_loss = -adv * torch.exp(log_prob - fixed_log_prob)
-                return act_loss.mean()
+        success, new_params = linesearch(ac.pi, get_loss, prev_params, fullstep, neggdotstepdir / lm[0])
 
-        success, new_params = linesearch(ac.pi, f, prev_params, fullstep,
-                                         neggdotstepdir / lm)
-
-        
-        # update policy
-        set_flat_params_to(ac.pi, new_params)# + b * pi_off_grad)
-
-
-def trpo_step(model, get_loss, get_kl, max_kl, damping):
-    loss = get_loss()
-    grads = torch.autograd.grad(loss, model.parameters())
-    loss_grad = torch.cat([grad.view(-1) for grad in grads]).data
-
-    def Fvp(v):
-        kl = get_kl()
-        kl = kl.mean()
-
-        grads = torch.autograd.grad(kl, model.parameters(), create_graph=True)
-        flat_grad_kl = torch.cat([grad.view(-1) for grad in grads])
-
-        kl_v = (flat_grad_kl * Variable(v)).sum()
-        grads = torch.autograd.grad(kl_v, model.parameters())
-        flat_grad_grad_kl = torch.cat([grad.contiguous().view(-1) for grad in grads]).data
-
-        return flat_grad_grad_kl + v * damping
-
-    stepdir = conjugate_gradients(Fvp, -loss_grad, 10)
-
-    shs = 0.5 * (stepdir * Fvp(stepdir)).sum(0, keepdim=True)
-
-    lm = torch.sqrt(shs / max_kl)
-    fullstep = stepdir / lm[0]
-
-    neggdotstepdir = (-loss_grad * stepdir).sum(0, keepdim=True)
-    print(("lagrange multiplier:", lm[0], "grad_norm:", loss_grad.norm()))
-
-    prev_params = get_flat_params_from(model)
-    success, new_params = linesearch(model, get_loss, prev_params, fullstep,
-                                     neggdotstepdir / lm[0])
-    
-    return loss, new_params
+        set_flat_params_to(ac.pi.mu_net, new_params)# + b * pi_off_grad)
